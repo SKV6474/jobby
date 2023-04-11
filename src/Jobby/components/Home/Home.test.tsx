0be18945b96264bc6fee9db2b3b6d9a3705694d1
SKV6474 <s9784114856k@gamil.com>
2023-04-11 17:37:42 +0530

    updated

--- src/Jobby/components/Home/Home.test.tsx
+++ src/Jobby/components/Home/Home.test.tsx
@@ -5,7 +5,7 @@ import renderer from "react-test-renderer";
 import Home from ".";
 
 const dom = new JSDOM("<!doctype html><html><body></body></html>");
-(global as any).window = dom.window as Window;
+(global as any).window = dom.window as unknown as Window;
 (global as any).document = dom.window.document;
 
 test("renders correctly", () => {
